9d2c7b4f9774acbe73dcdaf11ed90d6d6ebe18f5
iaboudou <ilyassaboudou07@gmail.com>
2026-05-17 14:03:51 +0100

    .sh file

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ROOT=$PWD
+
+install() {
+	cd $ROOT/server && npm install
+	cd $ROOT
+}
+
+server() {
+
+	echo "Server running at http://localhost:8080"
+	cd $ROOT/server && node src/index.js
+}
+
+client() {
+	echo "Client running at http://localhost:3000"
+	npx live-server $ROOT/client --port=3000 --quiet
+}
+
+run() {
+	server & client
+}
+
+all() {
+	install
+	server & client
+}
+
+$1
